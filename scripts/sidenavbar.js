89d650dcb95a92726f82b7edc8182b3b0c57b925
somesh2002 <somesh.agrawal6@gmail.com>
2022-02-15 22:31:13 +0530

    Skeleton Completed

---
+++ scripts/sidenavbar.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mubut").style.visibility = "hidden";
+  
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("mubut").style.visibility = "visible";
+  }
